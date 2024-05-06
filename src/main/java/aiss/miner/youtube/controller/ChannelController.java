55ae6b1940e2c188a607499c8c05f4e39e4a5bf0
Pepe <coritochico@gmail.com>
2024-05-06 14:16:25 +0200

    Added OAS

--- src/main/java/aiss/miner/youtube/controller/ChannelController.java
+++ src/main/java/aiss/miner/youtube/controller/ChannelController.java
@@ -3,6 +3,12 @@ package aiss.miner.youtube.controller;
 
 import aiss.miner.youtube.service.VideoMinerService;
 import aiss.miner.youtube.service.YoutubeTranslatedService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +22,36 @@ public class ChannelController {
     @Autowired
     YoutubeTranslatedService youtubeTranslatedService;
 
+    @Operation(
+            summary="Retrive a channel",
+            description="Get an existing Channel given an id.",
+            tags={"channel"}
+    )
+    @ApiResponses({ @ApiResponse(
+            responseCode="200",
+            content={@Content(
+                    array = @ArraySchema(schema = @Schema(implementation = Channel.class)),
+                    mediaType = "application/json"
+            )}
+    ),
+            @ApiResponse(
+                    responseCode="404",
+                    content={@Content(
+                            array = @ArraySchema(schema = @Schema())
+                    )}
+            ),
+            @ApiResponse(
+                    responseCode="403",
+                    content={@Content(
+                            array = @ArraySchema(schema = @Schema())
+                    )}
+            ),
+            @ApiResponse(
+                    responseCode="429",
+                    content={@Content(
+                            array = @ArraySchema(schema = @Schema())
+                    )}
+            )})
     @GetMapping("/{id}")
     public Channel findOne(@PathVariable String id,
                            @RequestParam(value="maxVideos", defaultValue ="10") String maxVideos,
@@ -24,6 +60,36 @@ public class ChannelController {
         return youtubeTranslatedService.getYoutubeChannel(id, Integer.parseInt(maxVideos), Integer.parseInt(maxComments));
     }
 
+    @Operation(
+            summary="Retrieve a channel and sends it",
+            description="Get an existing Channel given an id and send it to VideoMiner.",
+            tags={"channel"}
+    )
+    @ApiResponses({ @ApiResponse(
+            responseCode="200",
+            content={@Content(
+                    array = @ArraySchema(schema = @Schema(implementation = Channel.class)),
+                    mediaType = "application/json"
+            )}
+    ),
+            @ApiResponse(
+                    responseCode="404",
+                    content={@Content(
+                            array = @ArraySchema(schema = @Schema())
+                    )}
+            ),
+            @ApiResponse(
+                    responseCode="403",
+                    content={@Content(
+                            array = @ArraySchema(schema = @Schema())
+                    )}
+            ),
+            @ApiResponse(
+                    responseCode="429",
+                    content={@Content(
+                            array = @ArraySchema(schema = @Schema())
+                    )}
+            )})
     @PostMapping("/{id}")
     @ResponseStatus(HttpStatus.CREATED)
     public Channel postOne(@PathVariable String id,
