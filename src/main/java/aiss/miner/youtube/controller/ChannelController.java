a11592c9716103a83e8563ceb5c650e63ce3d8fa
miniluz <javiermelon4fu@gmail.com>
2024-05-06 11:51:16 +0200

    Add minimum and maximum to request parameters

--- src/main/java/aiss/miner/youtube/controller/ChannelController.java
+++ src/main/java/aiss/miner/youtube/controller/ChannelController.java
@@ -3,6 +3,8 @@ package aiss.miner.youtube.controller;
 
 import aiss.miner.youtube.service.VideoMinerService;
 import aiss.miner.youtube.service.YoutubeTranslatedService;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -18,8 +20,8 @@ public class ChannelController {
 
     @GetMapping("/{id}")
     public Channel findOne(@PathVariable String id,
-                           @RequestParam(value="maxVideos", defaultValue ="10") String maxVideos,
-                           @RequestParam(value="maxComments", defaultValue ="10") String maxComments)
+                           @RequestParam(value="maxVideos", defaultValue ="10") @Min(0) @Max(20) String maxVideos,
+                           @RequestParam(value="maxComments", defaultValue ="10") @Min(0) @Max(20) String maxComments)
     {
         return youtubeTranslatedService.getYoutubeChannel(id, Integer.parseInt(maxVideos), Integer.parseInt(maxComments));
     }
@@ -27,8 +29,8 @@ public class ChannelController {
     @PostMapping("/{id}")
     @ResponseStatus(HttpStatus.CREATED)
     public Channel postOne(@PathVariable String id,
-                           @RequestParam(value="maxVideos", defaultValue ="10") String maxVideos,
-                           @RequestParam(value="maxComments", defaultValue ="10") String maxComments)
+                           @RequestParam(value="maxVideos", defaultValue ="10") @Min(0) @Max(20) String maxVideos,
+                           @RequestParam(value="maxComments", defaultValue ="10") @Min(0) @Max(20) String maxComments)
     {
         Channel channel = youtubeTranslatedService.getYoutubeChannel(id, Integer.parseInt(maxVideos), Integer.parseInt(maxComments));
         videoMinerService.createChannel(channel);
